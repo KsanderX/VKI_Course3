5d3bcf0dfbdab4fec38011593c47a0e8ec5cc996
Саша Аганичев <aganichevAG@mer.ci.nsu.ru>
2025-10-01 22:34:48 +0700

    01.10

---
+++ 1_semester/Moduls/Lab2/Lab2/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnAddNewProduct(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
